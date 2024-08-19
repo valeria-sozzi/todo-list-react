d287fdb1ff0c88a44fe8a721e11d2eb24c1b1293
Valeria Sozzi <valeriasozzi24@gmail.com>
2024-08-19 13:06:26 +0200

    add todo list

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,74 @@
+import { useEffect, useState } from "react";
 import "./App.scss";
 
 function App() {
-  return;
-  <div className="app-container"></div>;
+  const [activities, setActivities] = useState([
+    { text: "Comprare il pane", completed: false },
+    { text: "Studiare React", completed: false },
+    { text: "Fare esercizio fisico", completed: true },
+  ]);
+
+  const [inputValue, setInputValue] = useState("ciao");
+
+  console.log("🚀 ~ App ~ activities:", activities);
+  return (
+    <div className="app-container">
+      <div className="new-activities">
+        <label htmlFor="activity" className="activity">
+          Inserisci la nuova attività
+        </label>
+        <input
+          onChange={(e) => {
+            setInputValue(e.target.value);
+          }}
+          type="text"
+          id="activity"
+          value={inputValue}
+        />
+        <button
+          className="btn"
+          onClick={() => {
+            const newActivity = { text: inputValue, completed: false };
+            const newActivities = [...activities, newActivity];
+            setActivities(newActivities);
+          }}
+        >
+          Aggiungi nuova attività
+        </button>
+      </div>
+      <ul>
+        {activities.map((activity, currentIndex) => (
+          <li key={currentIndex}>
+            <span
+              className={activity.completed ? "completed" : ""}
+              onClick={() => {
+                const newActivities = [...activities];
+                newActivities.map((activity, index) => {
+                  if (currentIndex === index) {
+                    activity.completed = !activity.completed;
+                  }
+                });
+                setActivities(newActivities);
+              }}
+            >
+              {activity.text}{" "}
+            </span>
+
+            <span
+              className="remove"
+              onClick={() => {
+                const newActivities = [...activities];
+                newActivities.splice(currentIndex, 1);
+                setActivities(newActivities);
+              }}
+            >
+              X
+            </span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
